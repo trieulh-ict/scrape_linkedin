5e884c0ec4e418295e170c6f5d05a9967577396d
Lê Hải Triều <trieulh.bk@gmail.com>
2018-06-19 22:35:20 +0700

    Test parallel * sqlalchemy

---
+++ database/setup.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+
+engine = create_engine('sqlite:///linkedin.db')
+
+Base.metadata.create_all(engine)
--- script/scrape_multiple_profiles.py
+++ script/scrape_multiple_profiles.py
@@ -1,17 +1,21 @@
 import json
 
 import selenium
-from scrape_linkedin import ProfileScraper
+from scrape_linkedin import scrape_in_parallel, ProfileScraper
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-cookie = '<Your LI_AT cookie>'
+cookie = '<>'
 
 options = Options()
 options.add_argument('--headless')
 HEADLESS_OPTIONS = {'chrome_options': options}
 
-with ProfileScraper(cookie=cookie, driver_options=HEADLESS_OPTIONS, driver=webdriver.Chrome) as scraper:
-    # https://www.linkedin.com/in/cuong-dao-duc-79978b107/
-    profile = scraper.scrape(user='hungtrv')
-print(json.dumps(profile.to_dict()))
+# with ProfileScraper(cookie=cookie, driver_options=HEADLESS_OPTIONS, driver=webdriver.Chrome) as scraper:
+#     # https://www.linkedin.com/in/cuong-dao-duc-79978b107/
+#     profile = scraper.scrape(user='hungtrv')
+# print(json.dumps(profile.to_dict()))
+
+profiles = ['hungtrv', 'cuong-dao-duc-79978b107']
+scrape_in_parallel(scraper_type=ProfileScraper, items=profiles, output_file='profiles.json', num_instances=2,
+                   cookie=cookie)
