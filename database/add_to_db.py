1058d2755de9f7ffd0cfda6e285d52a7d93a55e6
Lê Hải Triều <trieulh.bk@gmail.com>
2018-06-22 01:09:37 +0700

    add Skill table

---
+++ database/add_to_db.py
@@ -0,0 +1,42 @@
+import json
+from sqlalchemy import create_engine, exists
+from sqlalchemy.orm import sessionmaker
+from setup import Base, Skill, User, UserSkill
+
+engine = create_engine('mysql+pymysql://root:123456@localhost/linkedin')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+with open('profiles.json') as file:
+    data = json.load(file)
+
+    for user_id in data:
+        user = data[user_id]
+        user_profile = user['personal_info']
+
+        # Insert to db
+        new_user = User(linkedin_id=user_id)
+        session.add(new_user)
+
+        user_skills = user['skills']
+        for item in user_skills:
+            skill = session.query(Skill).filter(Skill.name == item['name']).first()
+            if not skill:
+                skill = Skill(name=item['name'])
+
+            user_skill = UserSkill(user=new_user, skill=skill, endorsement=int(item['endorsements']))
+            session.add(user_skill)
+            session.commit()
+
+        # session.commit()
--- database/setup.py
+++ database/setup.py
@@ -1,3 +1,5 @@
+import json
+
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -6,7 +8,33 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class UserSkill(Base):
+    __tablename__ = 'user_skill'
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    skill_id = Column(Integer, ForeignKey('skill.id'), primary_key=True)
+    endorsement = Column(Integer, default=0)
+    user = relationship("User", backref="user")
+    skill = relationship("Skill")
 
-engine = create_engine('mysql+pymysql://root:123456@localhost')
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    linkedin_id = Column(String(250), nullable=False)
+    name = Column(String(250))
+    school = Column(String(250))
+    headline = Column(String(250))
+    company = Column(String(250))
+    summary = Column(String(250))
+    location = Column(String(250))
+    skills = relationship("UserSkill")
+
+class Skill(Base):
+    __tablename__ = 'skill'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+engine = create_engine('mysql+pymysql://root:123456@localhost/linkedin')
 
 Base.metadata.create_all(engine)
