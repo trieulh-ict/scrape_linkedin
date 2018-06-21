51ec259682f4d37574b68da3b39084c1c6298b27
Lê Hải Triều <trieulh.bk@gmail.com>
2018-06-22 01:24:40 +0700

    Fix add multiple instance

--- database/add_to_db.py
+++ database/add_to_db.py
@@ -26,17 +26,22 @@ with open('profiles.json') as file:
         user_profile = user['personal_info']
 
         # Insert to db
-        new_user = User(linkedin_id=user_id)
-        session.add(new_user)
+        new_user = session.query(User).filter(User.linkedin_id == user_id).first()
+        if not new_user:
+            new_user = User(linkedin_id=user_id)
 
+        #Add Skill
         user_skills = user['skills']
         for item in user_skills:
             skill = session.query(Skill).filter(Skill.name == item['name']).first()
             if not skill:
                 skill = Skill(name=item['name'])
 
-            user_skill = UserSkill(user=new_user, skill=skill, endorsement=int(item['endorsements']))
-            session.add(user_skill)
-            session.commit()
+            if not session.query(UserSkill).filter(
+                    UserSkill.user_id == new_user.id and UserSkill.skill_id == skill.id).first():
+                user_skill = UserSkill(user=new_user, skill=skill, endorsement=int(item['endorsements']))
+                session.add(user_skill)
+
+        session.commit()
 
         # session.commit()
