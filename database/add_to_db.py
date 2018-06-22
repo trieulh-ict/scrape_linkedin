cea3b8bd1aedd8577edbe8d6223ab40fb577ba2d
Lê Hải Triều <trieulh.bk@gmail.com>
2018-06-23 01:41:36 +0700

    Add data to DB

--- database/add_to_db.py
+++ database/add_to_db.py
@@ -1,14 +1,14 @@
 import json
 from sqlalchemy import create_engine, exists
 from sqlalchemy.orm import sessionmaker
-from setup import Base, Skill, User, UserSkill
+from setup import Base, Skill, User, Accomplishment, Education, Job, Volunteering
 
-engine = create_engine('mysql+pymysql://root:123456@localhost/linkedin')
+engine = create_engine('mysql+pymysql://root:123456@localhost/linkedin?charset=utf8', encoding='utf-8')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind=engine)
+DBSession = sessionmaker(bind=engine, autoflush=False)
 # A DBSession() instance establishes all conversations with the database
 # and represents a "staging zone" for all the objects loaded into the
 # database session object. Any change made against the objects in the
@@ -29,19 +29,75 @@ with open('profiles.json') as file:
         new_user = session.query(User).filter(User.linkedin_id == user_id).first()
         if not new_user:
             new_user = User(linkedin_id=user_id)
+        else:
+            # Drop related skills
+            skills = session.query(Skill).filter(Skill.user_id == new_user.id).all()
+            for skill in skills:
+                session.delete(skill)
+            # Drop related accomplishments
+            accomps = session.query(Accomplishment).filter(Accomplishment.user_id == new_user.id).all()
+            for accomp in accomps:
+                session.delete(accomp)
+            # Drop related educations
+            edus = session.query(Education).filter(Education.user_id == new_user.id).all()
+            for edu in edus:
+                session.delete(edu)
+            # Drop related jobs
+            jobs = session.query(Job).filter(Job.user_id == new_user.id).all()
+            for job in jobs:
+                session.delete(job)
+            # Drop related volunteerings
+            volunteerings = session.query(Volunteering).filter(Volunteering.user_id == new_user.id).all()
+            for volunteering in volunteerings:
+                session.delete(volunteering)
 
-        #Add Skill
+            session.commit()
+
+        # Add Skills
         user_skills = user['skills']
         for item in user_skills:
-            skill = session.query(Skill).filter(Skill.name == item['name']).first()
-            if not skill:
-                skill = Skill(name=item['name'])
+            new_skill = Skill(name=item['name'])
+            new_user.skills.append(new_skill)
 
-            if not session.query(UserSkill).filter(
-                    UserSkill.user_id == new_user.id and UserSkill.skill_id == skill.id).first():
-                user_skill = UserSkill(user=new_user, skill=skill, endorsement=int(item['endorsements']))
-                session.add(user_skill)
+        # Add Accomplishments
+        user_accomps = user['accomplishments']
+        for category in user_accomps:
+            accomp_list = user_accomps[category]
+            for accomp in accomp_list:
+                new_accomp = Accomplishment(name=accomp, category=category)
+                new_user.accomplishments.append(new_accomp)
 
+        # Add Educations
+        user_educations = user['experiences']['education']
+        for edu in user_educations:
+            new_edu = Education(activities=edu['activities'],
+                                name=edu['name'],
+                                degree=edu['degree'],
+                                field_of_study=edu['field_of_study'],
+                                date_range=edu['date_range'],
+                                grades=edu['grades'])
+            new_user.educations.append(new_edu)
+
+        # Add Jobs
+        user_jobs = user['experiences']['jobs']
+        for job in user_jobs:
+            new_job = Job(company=job['company'],
+                          date_range=job['date_range'],
+                          title=job['title'],
+                          description=job['description'],
+                          location=job['location'])
+            new_user.jobs.append(new_job)
+
+        # Add Volunteerings
+        user_volunteerings = user['experiences']['volunteering']
+        for volunteering in user_volunteerings:
+            new_volunteering = Volunteering(company=volunteering['company'],
+                                            date_range=volunteering['date_range'],
+                                            title=volunteering['title'],
+                                            description=volunteering['description'],
+                                            location=volunteering['location'],
+                                            cause=volunteering['cause'])
+            new_user.volunteerings.append(new_volunteering)
+
+        session.merge(new_user)
         session.commit()
-
-        # session.commit()
--- database/setup.py
+++ database/setup.py
@@ -1,6 +1,6 @@
 import json
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,33 +8,73 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class UserSkill(Base):
-    __tablename__ = 'user_skill'
-    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    skill_id = Column(Integer, ForeignKey('skill.id'), primary_key=True)
-    endorsement = Column(Integer, default=0)
-    user = relationship("User", backref="user")
-    skill = relationship("Skill")
-
-
 class User(Base):
     __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    linkedin_id = Column(String(250), primary_key=True, nullable=False)
+    name = Column(Text)
+    school = Column(Text)
+    headline = Column(Text)
+    company = Column(Text)
+    summary = Column(Text)
+    location = Column(Text)
+    skills = relationship('Skill', backref='user')
+    accomplishments = relationship('Accomplishment', backref='user')
+    educations = relationship('Education', backref='user')
+    jobs = relationship('Job', backref='user')
+    volunteerings = relationship('Volunteering', backref='user')
+
+
+class Education(Base):
+    __tablename__ = 'education'
     id = Column(Integer, primary_key=True)
-    linkedin_id = Column(String(250), nullable=False)
-    name = Column(String(250))
-    school = Column(String(250))
-    headline = Column(String(250))
-    company = Column(String(250))
-    summary = Column(String(250))
-    location = Column(String(250))
-    skills = relationship("UserSkill")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    activities = Column(Text)
+    name = Column(Text)
+    degree = Column(Text)
+    field_of_study = Column(Text)
+    date_range = Column(Text)
+    grades = Column(Text)
+
+
+class Job(Base):
+    __tablename__ = 'job'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    company = Column(Text)
+    title = Column(Text)
+    description = Column(Text)
+    location = Column(Text)
+    date_range = Column(Text)
+
+
+class Volunteering(Base):
+    __tablename__ = 'volunteering'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    company = Column(Text)
+    title = Column(Text)
+    description = Column(Text)
+    location = Column(Text)
+    date_range = Column(Text)
+    cause = Column(Text)
+
 
 class Skill(Base):
     __tablename__ = 'skill'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    name = Column(Text, nullable=False)
 
 
-engine = create_engine('mysql+pymysql://root:123456@localhost/linkedin')
+class Accomplishment(Base):
+    __tablename__ = 'accomplishment'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    name = Column(Text, nullable=False)
+    category = Column(Text, nullable=False)
+
+
+engine = create_engine('mysql+pymysql://root:123456@localhost/linkedin?charset=utf8', encoding='utf-8')
 
 Base.metadata.create_all(engine)
